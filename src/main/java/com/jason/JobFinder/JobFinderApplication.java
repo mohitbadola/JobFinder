f88f91aa8faebda1c39d935d0b09545df7c2b166
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-14 00:25:09 +0530

    Initial commit

---
+++ src/main/java/com/jason/JobFinder/JobFinderApplication.java
@@ -0,0 +1,14 @@
+package com.jason.JobFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFinderApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
