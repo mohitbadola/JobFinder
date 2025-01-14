6c537bbc7c8de2b2de361561d7baca3a2bc1fa7d
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-15 02:23:42 +0530

    Create UsersController

--- src/main/java/com/jason/JobFinder/services/UsersTypeService.java
+++ src/main/java/com/jason/JobFinder/services/UsersTypeService.java
@@ -2,6 +2,7 @@ package com.jason.JobFinder.services;
 
 import com.jason.JobFinder.entity.UsersType;
 import com.jason.JobFinder.repository.UsersTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 public class UsersTypeService {
     private final UsersTypeRepository usersTypeRepository;
 
-
+    @Autowired
     public UsersTypeService(UsersTypeRepository usersTypeRepository) {
         this.usersTypeRepository = usersTypeRepository;
     }
