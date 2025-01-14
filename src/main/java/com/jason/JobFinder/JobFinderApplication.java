88bab45fa0d53142ee874684fe13051ca65f243b
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-15 02:53:31 +0530

    update application.properties

--- src/main/java/com/jason/JobFinder/JobFinderApplication.java
+++ src/main/java/com/jason/JobFinder/JobFinderApplication.java
@@ -8,7 +8,6 @@ public class JobFinderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobFinderApplication.class, args);
-		System.out.println("Hello world");
 	}
 
 }
