b4864f65cc80c0570695fc038cb568947cc2557c
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-15 02:16:46 +0530

    Create UsersTypeService.java

---
+++ src/main/java/com/jason/JobFinder/services/UsersTypeService.java
@@ -0,0 +1,21 @@
+package com.jason.JobFinder.services;
+
+import com.jason.JobFinder.entity.UsersType;
+import com.jason.JobFinder.repository.UsersTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersTypeService {
+    private final UsersTypeRepository usersTypeRepository;
+
+
+    public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+        this.usersTypeRepository = usersTypeRepository;
+    }
+
+    public List<UsersType> getAll(){
+        return usersTypeRepository.findAll();
+    }
+}
