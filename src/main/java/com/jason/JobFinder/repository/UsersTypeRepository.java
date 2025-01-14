99dede411a6571a9fb339696c0b630edb5717d7b
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-15 02:11:04 +0530

    Create UsersTypeRepository.java

---
+++ src/main/java/com/jason/JobFinder/repository/UsersTypeRepository.java
@@ -0,0 +1,8 @@
+package com.jason.JobFinder.repository;
+
+import com.jason.JobFinder.entity.UsersType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+
+}
