59f51ac058e40a830028140ed4c948448a741c2f
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-15 16:03:17 +0530

    Create UsersService.java

---
+++ src/main/java/com/jason/JobFinder/services/UsersService.java
@@ -0,0 +1,27 @@
+package com.jason.JobFinder.services;
+
+import com.jason.JobFinder.entity.Users;
+import com.jason.JobFinder.repository.UsersRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Users addNew(Users users){
+        users.setActive(true);
+        users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        return usersRepository.save(users);
+    }
+
+}
