3b94edd828a059166db408c31207d4a8e1c0cc74
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-15 15:43:32 +0530

    create post mapping for user registration

--- src/main/java/com/jason/JobFinder/controller/UsersController.java
+++ src/main/java/com/jason/JobFinder/controller/UsersController.java
@@ -3,10 +3,12 @@ package com.jason.JobFinder.controller;
 import com.jason.JobFinder.entity.Users;
 import com.jason.JobFinder.entity.UsersType;
 import com.jason.JobFinder.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,5 +28,11 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users){
+        System.out.println("User:: "+users);
+        return "dashboard";
     }
 }
