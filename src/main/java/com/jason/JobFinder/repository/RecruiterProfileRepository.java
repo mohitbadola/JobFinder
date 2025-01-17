e44946c0ee9c40b470a5f3bfd76ef6106a4a91fc
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-17 21:17:01 +0530

    Create RecruiterProfileRepository.java

---
+++ src/main/java/com/jason/JobFinder/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.jason.JobFinder.repository;
+
+import com.jason.JobFinder.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
