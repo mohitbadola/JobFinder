75c1d9138c096cce79b8db9ec0435a84bee62804
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-17 21:14:35 +0530

    Update Skills.java

--- src/main/java/com/jason/JobFinder/entity/Skills.java
+++ src/main/java/com/jason/JobFinder/entity/Skills.java
@@ -1,9 +1,26 @@
 package com.jason.JobFinder.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "skills")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Skills {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String experienceLevel;
+    private String yearsOfExperience;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job_seeker_profile")
+    private JobSeekerProfile jobSeekerProfile;
+
 }
