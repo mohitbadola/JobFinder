8b57af77f4da4953bb09eb13db74a5f24805da76
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-17 20:32:07 +0530

    Create RecruiterProfile.java

---
+++ src/main/java/com/jason/JobFinder/entity/RecruiterProfile.java
@@ -0,0 +1,33 @@
+package com.jason.JobFinder.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "recruiter_profile")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecruiterProfile {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String country;
+    private String company;
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+
+}
