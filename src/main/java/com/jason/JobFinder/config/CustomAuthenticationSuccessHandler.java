9b24d36aae42ca19fa793916bfb39432a0c07b0c
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-20 03:14:05 +0530

    Create CustomAuthenticationSuccessHandler.java

---
+++ src/main/java/com/jason/JobFinder/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,27 @@
+package com.jason.JobFinder.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        System.out.println("The username " + username + "is logged in");
+        boolean hasJobSeekerRole = authentication.getAuthorities().stream().anyMatch(r->r.getAuthority().equals("Job seeker"));
+        boolean hasRecruiter = authentication.getAuthorities().stream().anyMatch(r->r.getAuthority().equals("Recruiter"));
+        if(hasRecruiter || hasJobSeekerRole){
+            response.sendRedirect("/dashboard/");
+        }
+    }
+}
