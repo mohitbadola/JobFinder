b053a1bc2b22d0819d463f103c8da460bdb5f58a
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-20 01:38:00 +0530

    Create CustomUserDetailsService.java

---
+++ src/main/java/com/jason/JobFinder/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.jason.JobFinder.services;
+
+import com.jason.JobFinder.entity.Users;
+import com.jason.JobFinder.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found user"));
+        return null;
+    }
+}
