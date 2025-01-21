b99b2bfad029a78f7f5f3e9586539cd3d760523a
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-21 22:20:34 +0530

    Update JobPostActivityController.java

--- src/main/java/com/jason/JobFinder/controller/JobPostActivityController.java
+++ src/main/java/com/jason/JobFinder/controller/JobPostActivityController.java
@@ -1,4 +1,30 @@
 package com.jason.JobFinder.controller;
 
+import com.jason.JobFinder.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class JobPostActivityController {
+    private final UsersService usersService;
+
+    @Autowired
+    public JobPostActivityController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/dashboard")
+    public String searchJobs(Model model){
+        Object currentUserProfile = usersService.getCurrentUserProfile();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUserName = authentication.getName();
+            model.addAttribute("username", currentUserName);
+        }
+        model.addAttribute("user", currentUserProfile);
+        return "dashboard";
+    }
 }
