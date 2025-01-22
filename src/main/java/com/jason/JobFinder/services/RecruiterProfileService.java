9fc5ddbf14ed4837248677847990f05f16560ddd
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-22 20:45:48 +0530

    Create RecruiterProfileService.java

---
+++ src/main/java/com/jason/JobFinder/services/RecruiterProfileService.java
@@ -0,0 +1,23 @@
+package com.jason.JobFinder.services;
+
+import com.jason.JobFinder.entity.RecruiterProfile;
+import com.jason.JobFinder.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile>getOne(Integer id){
+        return recruiterProfileRepository.findById(id);
+    }
+
+}
