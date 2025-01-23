6efb9b9d32c6da4b047141ae433025851fc3d627
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-23 20:00:44 +0530

    Add functionality to save recruiter profiles with profile photos

--- src/main/java/com/jason/JobFinder/controller/RecruiterProfileController.java
+++ src/main/java/com/jason/JobFinder/controller/RecruiterProfileController.java
@@ -4,15 +4,21 @@ import com.jason.JobFinder.entity.RecruiterProfile;
 import com.jason.JobFinder.entity.Users;
 import com.jason.JobFinder.repository.UsersRepository;
 import com.jason.JobFinder.services.RecruiterProfileService;
+import com.jason.JobFinder.util.FileUploadUtil;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -28,16 +34,46 @@ public class RecruiterProfileController {
     }
 
     @GetMapping("/")
-    public String recruiterProfile(Model model){
+    public String recruiterProfile(Model model) {
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(!(authentication instanceof AnonymousAuthenticationToken)){
-            String currentUserName = authentication.getName();
-            Users users = usersRepository.findByEmail(currentUserName).orElseThrow(()->new UsernameNotFoundException("Could not "+ "found user"));
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could not " + "found user"));
             Optional<RecruiterProfile> recruiterProfile = recruiterProfileService.getOne(users.getUserId());
-            if(recruiterProfile.isEmpty())
+
+            if (!recruiterProfile.isEmpty())
                 model.addAttribute("profile", recruiterProfile.get());
+
         }
+
         return "recruiter_profile";
+    }
+
+    @PostMapping("/addNew")
+    public String addNew(RecruiterProfile recruiterProfile, @RequestParam("image")MultipartFile multipartFile, Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUserName).orElseThrow(() -> new UsernameNotFoundException("Could not " + "found user"));
+            recruiterProfile.setUserId(users);
+            recruiterProfile.setUserAccountId(users.getUserId());
+        }
+            model.addAttribute("profile", recruiterProfile);
+            String fileName = "";
+            if(!multipartFile.getOriginalFilename().equals("")){
+                fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+                recruiterProfile.setProfilePhoto(fileName);
+            }
+            RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
+        String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();
+            try{
+                FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        return "redirect:/dashboard";
     }
 
 }
