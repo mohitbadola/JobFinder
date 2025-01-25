f850c05166f91a7f7028f207df60d77f8114ada6
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-25 21:43:08 +0530

    Create JobPostActivityRepository.java

---
+++ src/main/java/com/jason/JobFinder/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.jason.JobFinder.repository;
+
+import com.jason.JobFinder.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
