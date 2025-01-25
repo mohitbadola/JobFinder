19ad5cbb9231d26dec4f660ab88075e3293c9b0e
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-25 21:42:54 +0530

    Create JobPostActivity.java

---
+++ src/main/java/com/jason/JobFinder/entity/JobPostActivity.java
@@ -0,0 +1,51 @@
+package com.jason.JobFinder.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobPostActivity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer jobPostId;
+
+    @ManyToOne
+    @JoinColumn(name = "postById", referencedColumnName = "userId")
+    private Users postedById;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "jobLocationId", referencedColumnName = "Id")
+    private JobLocation jobLocationId;
+
+    @ManyToOne
+    @JoinColumn(name = "jobCompanyId", referencedColumnName = "Id")
+    private JobCompany jobCompanyId;
+
+    @Transient
+    private Boolean isActive;
+
+    @Transient
+    private Boolean isSaved;
+
+    @Length(max = 10000)
+    private String descriptionOfJob;
+
+    private String jobType;
+    private String salary;
+    private String remote;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date postedDate;
+    private String jobTitle;
+
+}
