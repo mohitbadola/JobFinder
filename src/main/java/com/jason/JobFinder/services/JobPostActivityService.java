45662e63712ab2fda854d7152b81b10e1d145b8d
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-26 17:00:25 +0530

    Added getRecruiterJobs method to fetch and transform recruiter job details.

--- src/main/java/com/jason/JobFinder/services/JobPostActivityService.java
+++ src/main/java/com/jason/JobFinder/services/JobPostActivityService.java
@@ -1,8 +1,11 @@
 package com.jason.JobFinder.services;
 
-import com.jason.JobFinder.entity.JobPostActivity;
+import com.jason.JobFinder.entity.*;
 import com.jason.JobFinder.repository.JobPostActivityRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -15,5 +18,16 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity){
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+        for(IRecruiterJobs rec : recruiterJobsDtos){
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(), rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
     }
 }
