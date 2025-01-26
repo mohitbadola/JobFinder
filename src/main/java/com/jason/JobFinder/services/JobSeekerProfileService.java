55a104c7816d94aa11628e72c56ab02c8ccef478
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-26 19:47:56 +0530

    Create JobSeekerProfileService.java

---
+++ src/main/java/com/jason/JobFinder/services/JobSeekerProfileService.java
@@ -0,0 +1,22 @@
+package com.jason.JobFinder.services;
+
+import com.jason.JobFinder.entity.JobSeekerProfile;
+import com.jason.JobFinder.repository.JobSeekerProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id){
+        return jobSeekerProfileRepository.findById(id);
+    }
+
+}
