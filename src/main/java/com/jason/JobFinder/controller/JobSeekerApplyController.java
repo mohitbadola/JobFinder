df5fbf8c2520a44fdf0049d86b83797f7db9452f
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-26 17:20:04 +0530

    Create JobSeekerApplyController

--- src/main/java/com/jason/JobFinder/controller/JobSeekerApplyController.java
+++ src/main/java/com/jason/JobFinder/controller/JobSeekerApplyController.java
@@ -1,4 +1,30 @@
 package com.jason.JobFinder.controller;
 
+import com.jason.JobFinder.entity.JobPostActivity;
+import com.jason.JobFinder.services.JobPostActivityService;
+import com.jason.JobFinder.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class JobSeekerApplyController {
+
+    private final JobPostActivityService jobPostActivityService;
+    private final UsersService usersService;
+
+    @Autowired
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService) {
+        this.jobPostActivityService = jobPostActivityService;
+        this.usersService = usersService;
+    }
+
+    @GetMapping("job-details-apply/{id}")
+    public String display(@PathVariable("id") int id, Model model){
+        JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+        model.addAttribute("jobDetails", jobDetails);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "job-details";
+
+    }
 }
--- src/main/java/com/jason/JobFinder/services/JobPostActivityService.java
+++ src/main/java/com/jason/JobFinder/services/JobPostActivityService.java
@@ -16,18 +16,22 @@ public class JobPostActivityService {
         this.jobPostActivityRepository = jobPostActivityRepository;
     }
 
-    public JobPostActivity addNew(JobPostActivity jobPostActivity){
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
     }
 
-    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
         List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
         List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
-        for(IRecruiterJobs rec : recruiterJobsDtos){
+        for (IRecruiterJobs rec : recruiterJobsDtos) {
             JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
             JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
             recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(), rec.getJob_title(), loc, comp));
         }
         return recruiterJobsDtoList;
+    }
+
+    public JobPostActivity getOne(int id) {
+        return jobPostActivityRepository.findById(id).orElseThrow(() -> new RuntimeException("Job not found"));
     }
 }
