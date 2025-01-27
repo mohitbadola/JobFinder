c89c157a50d49fe17d60773d7c9ae81190a54385
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-27 18:47:11 +0530

    Create JobSeekerApply.java

---
+++ src/main/java/com/jason/JobFinder/entity/JobSeekerApply.java
@@ -0,0 +1,35 @@
+package com.jason.JobFinder.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"userId", "job"})})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobSeekerApply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job", referencedColumnName = "jobPostId")
+    private JobPostActivity job;
+
+    @DateTimeFormat(pattern = "dd-MM-YYYY")
+    private Date applyDate;
+
+    private String coverLetter;
+
+}
