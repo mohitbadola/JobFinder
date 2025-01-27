25def68f587b794d3e79a6a85751f28422b2406d
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-27 21:10:16 +0530

    Enhance job search functionality with advanced filters and default criteria.

--- src/main/java/com/jason/JobFinder/controller/JobPostActivityController.java
+++ src/main/java/com/jason/JobFinder/controller/JobPostActivityController.java
@@ -1,10 +1,8 @@
 package com.jason.JobFinder.controller;
 
-import com.jason.JobFinder.entity.JobPostActivity;
-import com.jason.JobFinder.entity.RecruiterJobsDto;
-import com.jason.JobFinder.entity.RecruiterProfile;
-import com.jason.JobFinder.entity.Users;
+import com.jason.JobFinder.entity.*;
 import com.jason.JobFinder.services.JobPostActivityService;
+import com.jason.JobFinder.services.JobSeekerApplyService;
 import com.jason.JobFinder.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -13,26 +11,96 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class JobPostActivityController {
     private final UsersService usersService;
     private final JobPostActivityService jobPostActivityService;
+    private final JobSeekerApplyService jobSeekerApplyService;
 
     @Autowired
-    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService, JobSeekerApplyService jobSeekerApplyService) {
         this.usersService = usersService;
         this.jobPostActivityService = jobPostActivityService;
+        this.jobSeekerApplyService = jobSeekerApplyService;
     }
 
     @GetMapping("/dashboard")
-    public String searchJobs(Model model) {
+    public String searchJobs(Model model, @RequestParam(value = "job", required = false) String job,
+                             @RequestParam(value = "location", required = false) String location,
+                             @RequestParam(value = "partTime", required = false) String partTime,
+                             @RequestParam(value = "fullTime", required = false) String fullTime,
+                             @RequestParam(value = "freelance", required = false) String freelance,
+                             @RequestParam(value = "remoteOnly", required = false) String remoteOnly,
+                             @RequestParam(value = "officeOnly", required = false) String officeOnly,
+                             @RequestParam(value = "partialRemote", required = false) String partialRemote,
+                             @RequestParam(value = "today", required = false) boolean today,
+                             @RequestParam(value = "days7", required = false) boolean days7,
+                             @RequestParam(value = "days30", required = false) boolean days30) {
+
+        model.addAttribute("partTime", Objects.equals(partTime, "Part-Time"));
+        model.addAttribute("fullTime", Objects.equals(fullTime, "Full-Time"));
+        model.addAttribute("freelance", Objects.equals(freelance, "Freelance"));
+
+        model.addAttribute("remoteOnly", Objects.equals(remoteOnly, "Remote-Only"));
+        model.addAttribute("officeOnly", Objects.equals(officeOnly, "Office-Only"));
+        model.addAttribute("partialRemote", Objects.equals(partialRemote, "Partial-Remote"));
+
+        model.addAttribute("today", Objects.equals(today, "today"));
+        model.addAttribute("day7", Objects.equals(days7, "days7"));
+        model.addAttribute("days30", Objects.equals(days30, "days30"));
+
+        model.addAttribute("job", Objects.equals(job, "job"));
+        model.addAttribute("location", Objects.equals(location, "location"));
+
+        LocalDate searchDate = null;
+        List<JobPostActivity> jobPost = null;
+        boolean dateSearchFlag = true;
+        boolean remote = true;
+        boolean type = true;
+
+        if (days30) {
+            searchDate = LocalDate.now().minusDays(30);
+        } else if (days7) {
+            searchDate = LocalDate.now().minusDays(7);
+        } else if (today) {
+            searchDate = LocalDate.now();
+        } else {
+            dateSearchFlag = false;
+        }
+
+        if (partTime == null && fullTime == null && freelance == null) {
+            partTime = "Part-Time";
+            fullTime = "Full-Time";
+            freelance = "Freelance";
+            remote = false;
+        }
+
+        if (officeOnly == null && remoteOnly == null && partialRemote == null) {
+            officeOnly = "Office-Only";
+            remoteOnly = "Remote-Only";
+            partialRemote = "Partial-Remote";
+            type = false;
+        }
+
+        if (!dateSearchFlag && !remote && !type && !StringUtils.hasText(job) && !StringUtils.hasText(location)) {
+            jobPost = jobPostActivityService.getAll();
+        } else {
+            jobPost = jobPostActivityService.search(job, location, Arrays.asList(partTime, fullTime, freelance),
+                    Arrays.asList(remoteOnly, officeOnly, partialRemote), searchDate);
+        }
+
 
         Object currentUserProfile = usersService.getCurrentUserProfile();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -43,12 +111,14 @@ public class JobPostActivityController {
         }
 
         model.addAttribute("user", currentUserProfile);
-        if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
             List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
             model.addAttribute("jobPost", recruiterJobs);
+        }else{
+            List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getCandidatesJobs((JobSeekerProfile) currentUserProfile);
         }
 
-        model.addAttribute("message", "Welcome to the Dashboard!");
+        model.addAttribute("user", currentUserProfile);
         return "dashboard";
     }
 
@@ -73,7 +143,7 @@ public class JobPostActivityController {
     }
 
     @PostMapping("dashboard/edit/{id}")
-    public String editJob(@PathVariable("id") int id, Model model){
+    public String editJob(@PathVariable("id") int id, Model model) {
         JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
         model.addAttribute("jobPostActivity", jobPostActivity);
         model.addAttribute("user", usersService.getCurrentUserProfile());
