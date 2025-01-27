0c7f187e309269deab1f22b03dd370cd9b11d8eb
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-27 21:35:21 +0530

    Add logic to set job application and save status for job posts.

--- src/main/java/com/jason/JobFinder/controller/JobPostActivityController.java
+++ src/main/java/com/jason/JobFinder/controller/JobPostActivityController.java
@@ -3,6 +3,7 @@ package com.jason.JobFinder.controller;
 import com.jason.JobFinder.entity.*;
 import com.jason.JobFinder.services.JobPostActivityService;
 import com.jason.JobFinder.services.JobSeekerApplyService;
+import com.jason.JobFinder.services.JobSeekerSaveService;
 import com.jason.JobFinder.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -28,12 +29,14 @@ public class JobPostActivityController {
     private final UsersService usersService;
     private final JobPostActivityService jobPostActivityService;
     private final JobSeekerApplyService jobSeekerApplyService;
+    private final JobSeekerSaveService jobSeekerSaveService;
 
     @Autowired
-    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService, JobSeekerApplyService jobSeekerApplyService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService, JobSeekerApplyService jobSeekerApplyService, JobSeekerSaveService jobSeekerSaveService) {
         this.usersService = usersService;
         this.jobPostActivityService = jobPostActivityService;
         this.jobSeekerApplyService = jobSeekerApplyService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
     }
 
     @GetMapping("/dashboard")
@@ -116,6 +119,36 @@ public class JobPostActivityController {
             model.addAttribute("jobPost", recruiterJobs);
         }else{
             List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getCandidatesJobs((JobSeekerProfile) currentUserProfile);
+            List<JobSeekerSave> jobSeekerSaveList = jobSeekerSaveService.getCandidatesJob((JobSeekerProfile) currentUserProfile);
+
+            boolean exist;
+            boolean saved;
+
+            for(JobPostActivity jobActivity : jobPost){
+                exist = false;
+                saved = false;
+                for(JobSeekerApply jobSeekerApply : jobSeekerApplyList){
+                    if(Objects.equals(jobActivity.getJobPostId(), jobSeekerApply.getJob().getJobPostId())){
+                        jobActivity.setIsActive(true);
+                        exist = true;
+                        break;
+                    }
+                }
+                for(JobSeekerSave jobSeekerSave : jobSeekerSaveList){
+                    if(Objects.equals(jobActivity.getJobPostId(), jobSeekerSave.getJob().getJobPostId())){
+                        jobActivity.setIsSaved(true);
+                        saved =true;
+                        break;
+                    }
+                }
+                if(!exist){
+                    jobActivity.setIsActive(false);
+                }
+                if(!saved){
+                    jobActivity.setIsSaved(false);
+                }
+                model.addAttribute("jobPost", jobPost);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
