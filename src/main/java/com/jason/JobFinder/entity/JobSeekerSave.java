ec1085e6288c55e0b4c4a3a75657263219346f53
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-27 19:00:35 +0530

    Create JobSeekerSave.java

---
+++ src/main/java/com/jason/JobFinder/entity/JobSeekerSave.java
@@ -0,0 +1,29 @@
+package com.jason.JobFinder.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"userId", "job"})})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobSeekerSave implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job", referencedColumnName = "jobPostId")
+    private JobPostActivity job;
+
+}
